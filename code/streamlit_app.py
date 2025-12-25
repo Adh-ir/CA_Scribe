65b5af7308f0bc2e439027190ca5203cab962639
Adhir Singh <adhirsingh@Adhirs-MacBook-Air.local>
2025-12-25 08:31:22 +0200

    Reduce model-button spacing to 5px

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -327,7 +327,7 @@ def show_main_page():
             }.get(x, x)
         )
         
-        st.markdown("<div style='height: 10px;'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='height: 5px;'></div>", unsafe_allow_html=True)
         
         # --- FRAGMENT: Isolate button rerun to prevent container flash ---
         @st.fragment
